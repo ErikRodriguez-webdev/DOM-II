42cd2a198230f821a8319e19ae169683ceff86b7
Erik Rodriguez <eric19.21.03@gmail.com>
2020-01-14 16:20:34 -0800

    completed mvp

--- js/index.js
+++ js/index.js
@@ -11,10 +11,11 @@ navH1removes.addEventListener("mouseleave", () => {
   navH1removes.style.color = "black";
 });
 
-// 3
+// 3 PROPAGATION part 1
 document.querySelectorAll("img").forEach(function(item) {
-  item.addEventListener("click", () => {
+  item.addEventListener("click", (event) => {
     item.style.transform = "scale(1.5)";
+    event.stopPropagation();
   });
 });
 
@@ -40,8 +41,47 @@ document.querySelectorAll(".btn").forEach(function(theBtns) {
 });
 
 // 7
-document.querySelector("h2").forEach(function(allH2) {
-  allH2.addEventListener("wheel", () => {
-    allH2.style.transform = "rotate(1turn)";
+const firstH2 = document.querySelector(".intro h2");
+firstH2.addEventListener("wheel", () => {
+  firstH2.style.textAlign = "center";
+});
+
+// 8
+const theHeader = document.querySelector(".nav-container");
+theHeader.addEventListener("contextmenu", () => {
+  gsap.to(".nav-container", {
+    rotate: 180,
+    yoyo: true,
+    repeat: 1
   });
+});
+
+// 9
+const anImageAdd = document.querySelector(".content-destination img");
+anImageAdd.addEventListener("mouseover", () => {
+  anImageAdd.style.border = "10px solid red";
+});
+
+// 10
+const anImageRemove = document.querySelector(".content-destination img");
+anImageRemove.addEventListener("mouseout", () => {
+  anImageRemove.style.border = "0px solid red";
+});
+
+// propagation here part 2
+const bg = document.querySelector("body");
+bg.addEventListener("click", () => {
+  bg.style.backgroundColor = "green";
+});
+
+//preventdefault here part 3
+const addLink = document.querySelector(".nav-link");
+addLink.href = "http://www.lambdaschool.com/";
+//console.log(addLink);
+
+const stopNewLink = document.querySelector(".nav-link");
+stopNewLink.addEventListener("click", (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  console.log("link has been prevented");
 });
